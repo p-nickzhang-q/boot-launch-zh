02ee0c247450f57e7b49805d26cd63a229fea7f2
zhao <zhao@yicksolutions.com>
2020-07-25 17:14:57 +0800

    配置属性数据校验

--- src/main/java/com/zh/boot/launch/entities/Family.java
+++ src/main/java/com/zh/boot/launch/entities/Family.java
@@ -3,13 +3,21 @@ package com.zh.boot.launch.entities;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @Component
+/**
+ * 使对象验证注解生效
+ */
+@Validated
 @ConfigurationProperties(prefix = "family")
 public class Family {
 
     //    @Value("${family.family-name}")
+    @NotEmpty
     private String familyName;
     private Father father;
     private Mother mother;
--- src/main/java/com/zh/boot/launch/entities/Father.java
+++ src/main/java/com/zh/boot/launch/entities/Father.java
@@ -2,9 +2,11 @@ package com.zh.boot.launch.entities;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 @Data
 public class Father {
     private String name;
-
+    @Min(21)
     private Integer age;
 }
