95571dad41be41ecec504ae31b7a0c10fb690e12
zhao <zhao@yicksolutions.com>
2020-08-01 14:00:25 +0800

    监听器，过滤器实现

--- src/main/java/com/zh/boot/launch/BootLaunchApplication.java
+++ src/main/java/com/zh/boot/launch/BootLaunchApplication.java
@@ -3,12 +3,14 @@ package com.zh.boot.launch;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 /**
  * ImportResource
  * 注入xml的bean
  */
+@ServletComponentScan
 @SpringBootApplication
 @MapperScan(basePackages = {"com.zh.boot.launch.mapper"})
 @ImportResource(locations = {"classpath:beans.xml"})
---
+++ src/main/java/com/zh/boot/launch/config/Customlister.java
@@ -0,0 +1,26 @@
+package com.zh.boot.launch.config;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * 监听器
+ */
+@WebListener
+@Slf4j
+public class Customlister implements ServletRequestListener {
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        log.info(" request监听器：销毁");
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        log.info(" request监听器：可以在这里记录访问次数哦");
+    }
+
+}
